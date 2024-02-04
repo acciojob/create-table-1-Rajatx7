45a2c7b23e2278fe664c33e32110c85f6774277a
Rajat Singh <155432801+Rajatx7@users.noreply.github.com>
2024-02-04 12:02:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function insert_Row() {
     //Write your code here
 	let tab = document.getElementById("sampleTable");
-	tab.innerHTML = `<tr><td>New cell1</td> 
-					<td>New cell2</td></tr> ` + tab.innerHTML
+	tab.innerHTML = `<tr><td>New Cell1</td> 
+					<td>New Cell2</td></tr> ` + tab.innerHTML
 	// c++;
   
 }
